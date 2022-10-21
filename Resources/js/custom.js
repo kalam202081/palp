166c8dce4b326b07aefab93875f34f74170fa06f
kalam202081 <abulkalama571@gmail.com>
2022-10-21 10:59:06 +0600

    first commit

---
+++ Resources/js/custom.js
@@ -0,0 +1,11 @@
+function openMenu(){
+	document.getElementById('nav-bar').style.width="100%";
+}
+function closeMenu(){
+	document.getElementById('nav-bar').style.width="0%";
+}
+
+
+new WOW().init();
+
+var mixer = mixitup('.work-filter');
